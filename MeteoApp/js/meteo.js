42134506537e93e18182274dfa322303fb2f81ca
prandigno89 <prandigno89@gmail.com>
2016-04-13 13:27:05 +0200

    Master Edition Meteo
    Versione applicazione per chiamata meteo

---
+++ MeteoApp/js/meteo.js
@@ -0,0 +1,16 @@
+var key = "3514e99dafd840c206746328103ff0a9";
+$(//start zepto
+	
+	function(){	
+    }
+    
+       
+    
+);//end zepto
+
+//Funzione per la chiamata meteo
+function chiamaMeteo(){
+    
+    
+    
+}
