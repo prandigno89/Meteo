8b9b288ddfeaafef348878bef6d3136219156230
prandigno89 <prandigno89@gmail.com>
2016-04-13 14:03:37 +0200

    Modifica 1
    Rimossi i servizi inutili

--- MeteoApp/js/meteo.js
+++ MeteoApp/js/meteo.js
@@ -1,7 +1,11 @@
 var key = "3514e99dafd840c206746328103ff0a9";
 $(//start zepto
 	
-	function(){	
+	function(){
+        
+        $('#meteo').on('click', function(){chiamaMeteo()});
+        
+        	
     }
     
        
@@ -10,7 +14,20 @@ $(//start zepto
 
 //Funzione per la chiamata meteo
 function chiamaMeteo(){
+    //chiamata:http://api.openweathermap.org/data/2.5/weather?q=Milan&appid=
+    var url= "http://api.openweathermap.org/data/2.5/weather?q=Milano&units=metric&appid=";
+    
+    $.getJSON(url+key, function(data){
+    console.log(data)
+    var city = data.name;
+    var descrizione = data.weather[0].description;
+    var icona = data.weather[0].icon;
+    var temperatura = data.main.temp;
+    //var nome = data.main.name
+    console.log(city + ", "+ descrizione + ", " + icona + ", "+ temperatura);
+    //alert(descrizione + ", "+ icona + ", " + nome);
     
     
+})
     
 }
